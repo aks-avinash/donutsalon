dae4181606a803180d169768744522f8f1a0306e
tedsuo <ted@lightstep.com>
2017-12-05 16:27:00 -0800

    foo

--- start_back.sh
+++ start_back.sh
@@ -1,2 +1,3 @@
+envsubst </etc/jaeger.yaml >/etc/envoy-jaeger.yaml
 donutbin --service_hostport="front-envoy:80" &
 envoy -c /etc/envoy.json --service-cluster donutsalon-${SERVICE_NAME} --service-node `hostname`
--- start_front.sh
+++ start_front.sh
@@ -1,1 +1,2 @@
+envsubst </etc/jaeger.yaml >/etc/envoy-jaeger.yaml
 envoy -c /etc/envoy.json --service-cluster front-proxy --service-node `hostname`
